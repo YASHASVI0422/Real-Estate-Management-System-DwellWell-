2827b7d3f2462c3e605e348ff34f5d60a33c6cd8
Yashasvi Pandey <127979114+YASHASVI0422@users.noreply.github.com>
2025-03-19 18:13:48 +0530

    adding files

---
+++ about.php
@@ -0,0 +1,94 @@
+<?php 
+ini_set('session.cache_limiter','public');
+session_cache_limiter(false);
+session_start();
+include("config.php");								
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="Real Estate PHP">
+<meta name="keywords" content="">
+<meta name="author" content="Unicoder">
+<link rel="shortcut icon" href="images/favicon.ico">
+<link href="https://fonts.googleapis.com/css?family=Muli:400,400i,500,600,700&amp;display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Comfortaa:400,700" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-slider.css">
+<link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
+<link rel="stylesheet" type="text/css" href="css/layerslider.css">
+<link rel="stylesheet" type="text/css" href="css/color.css" id="color-change">
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="fonts/flaticon/flaticon.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+
+<title>Real Estate PHP</title>
+</head>
+<body>
+
+<div id="page-wrapper">
+    <div class="row"> 
+		<?php include("include/header.php");?>
+        <div class="full-row">
+            <div class="container">
+                
+				
+				<?php 
+					
+					$query=mysqli_query($con,"SELECT * FROM about");
+					while($row=mysqli_fetch_array($query))
+					{
+				?>
+				<div class="row">
+                    <div class="col-md-12 col-lg-12">
+                        <h3 class="double-down-line-left text-secondary position-relative pb-4 mb-4"><?php echo $row['1'];?></h3>
+                    </div>
+                </div>
+                <div class="row about-company">
+                    <div class="col-md-12 col-lg-7">
+                        <div class="about-content">
+                        DwellWell is an innovative real estate management platform designed to revolutionize the way individuals and businesses engage with property transactions.
+                        Our mission is to simplify and enhance the experience of buying, renting, and managing properties for homeowners, tenants, real estate agents, and property managers alike.
+                        Founded by a dedicated team comprising Aman Chhimwal, Mahek Aggarwal, Yashasvi Pandey, and Harsh Teotia, we are committed to delivering a comprehensive, user-friendly solution that streamlines operations and fosters transparency in the real estate market.
+                        Our platform features a robust web application that allows users to effortlessly book PG accommodations, buy and sell properties, and explore listings through immersive 360-degree views. We prioritize user satisfaction by incorporating essential features such as detailed property listings, secure payment integration, property comparison tools, and a notification system that keeps users informed about new listings and updates. Additionally, our admin dashboard empowers property managers to efficiently oversee listings and user interactions, ensuring a smooth and organized experience for all parties involved.
+                        At DwellWell, we believe in leveraging technology to create a seamless and efficient real estate management experience, making property transactions accessible and straightforward for everyone. Our commitment to excellence and innovation positions us as a trusted partner in the real estate journey, dedicated to meeting the diverse needs of our users while enhancing the overall property management landscape.
+                        </div>
+                    </div>
+                    <div class="col-md-12 col-lg-5 mt-5">
+                        <div class="about-img"> <img src="admin/assets/upload/<?php echo $row['3'];?>" alt="about image"> </div>
+                    </div>
+                </div>
+				
+				<?php } ?>
+				
+            </div>
+        </div>
+		<?php include("include/footer.php");?>
+        <a href="#" class="bg-secondary text-white hover-text-secondary" id="scroll"><i class="fas fa-angle-up"></i></a> 
+</div>
+
+
+<script src="js/jquery.min.js"></script> 
+
+<script src="js/greensock.js"></script> 
+<script src="js/layerslider.transitions.js"></script> 
+<script src="js/layerslider.kreaturamedia.jquery.js"></script> 
+<script src="js/popper.min.js"></script> 
+<script src="js/bootstrap.min.js"></script> 
+<script src="js/owl.carousel.min.js"></script> 
+<script src="js/tmpl.js"></script> 
+<script src="js/jquery.dependClass-0.1.js"></script> 
+<script src="js/draggable-0.1.js"></script> 
+<script src="js/jquery.slider.js"></script> 
+<script src="js/wow.js"></script> 
+<script src="js/jquery.cookie.js"></script> 
+<script src="js/custom.js"></script>
+</body>
+
+</html>
